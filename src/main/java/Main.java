51736d85ada8285375e418a135b149ba34117e53
KamyabTabani <k.tabani82@gmail.com>
2021-04-13 13:50:16 +0430

    Methods Of database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
-public class Main{
-	public static void main(String[] args){
-		// program
+
+public class Main {
+	public static void main(String[] args) {
+		// User u = new User("Mark", "Antony", "142");
+		DBConnector.Connect();
+		// DBConnector.AddUser(u);
+		System.out.println(DBConnector.ConvertuserToArrayList(DBConnector.GetAllUsers()));
 	}
 }
